08d4c9a72174d3085fdd4b174d4ba8a795a56866
kharioki <antolisah@gmail.com>
2020-07-26 01:45:40 +0300

    receive calls and make calls and set video stream

--- public/script.js
+++ public/script.js
@@ -17,15 +17,43 @@ navigator.mediaDevices
   })
   .then(stream => {
     addVideoStream(myVideo, stream);
+
+    // listen to when someone needs to call us
+    myPeer.on('call', call => {
+      // answer their call and send them our stream
+      call.answer(stream);
+
+      // create a video stream
+      const video = document.createElement('video');
+      call.on('stream', userVideoStream => {
+        addVideoStream(video, userVideoStream);
+      });
+    });
+
+    // allow other users to connect
+    socket.on('user-connected', userId => {
+      connectToNewUser(userId, stream);
+    });
   });
 
 myPeer.on('open', id => {
   socket.emit('join-room', ROOM_ID, id);
 });
 
-socket.on('user-connected', userId => {
-  console.log('User connected: ' + userId);
-});
+// connect to new user function
+function connectToNewUser(userId, stream) {
+  // calling user and sending them our video and audio stream
+  const call = myPeer.call(userId, stream);
+  const video = document.createElement('video');
+  call.on('stream', userVideoStream => {
+    addVideoStream(video, userVideoStream);
+  });
+
+  // listen to a close event and remove video
+  call.on('close', () => {
+    video.remove();
+  });
+}
 
 // add video stream function
 function addVideoStream(video, stream) {
